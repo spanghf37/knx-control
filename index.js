3f6022c02b18ddbac14ee10e668314041199f718
spanghf37 <spanghf37@gmail.com>
2017-10-24 20:11:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ var myknxconnection = knx.Connection({
 		event: function(evt, src, dest, value) {
 			console.log('*** knx.Connection event : ' + evt.toString() + ' source : ' + src.toString() + ' destination : ' + dest.toString() + ' hex value : ' + value.toString('hex'));
 			module_myknx.insert_emoncms(evt, src, dest, value);
+			module_myknx.insert_emoncmsbis(evt, src, dest, value);
 		},
 		error: function(connstatus) {
 			console.log("**** ERROR: %j", connstatus);
